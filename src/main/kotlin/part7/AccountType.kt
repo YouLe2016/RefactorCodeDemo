e6c096c96e1a8153ca362926ab7505bbaadc6b6a
江心才子-攀升 <270628297@qq.com>
2025-03-11 23:11:54 +0800

    Add: Move Method(重构)

--- src/main/kotlin/part7/AccountType.kt
+++ src/main/kotlin/part7/AccountType.kt
@@ -1,13 +1,35 @@
 package part7
 
+import java.math.BigDecimal
+import java.math.MathContext
+import java.math.RoundingMode
+
 const val AccountType_Senior = 1
 const val AccountType_Normal = AccountType_Senior + 1
 
 class AccountType(
     private val type: Int,
-    var interestRate: Double
+    val interestRate: Double,
 ) {
     fun isPremium(): Boolean {
         return type == AccountType_Senior
+    }
+
+    fun calcOverdraftCharge(dayOverdrawn: Int): Double {
+        if (isPremium()) {
+            if (dayOverdrawn > 7) {
+                return 10.0 + (dayOverdrawn - 7) * 0.85
+            }
+            return 10.0
+        }
+        return dayOverdrawn * 1.75
+    }
+
+    fun calculateInterestRate(amount: Int, days: Int): BigDecimal {
+        return if (isPremium()) {
+            (interestRate * amount * days / 360).toBigDecimal(MathContext(2, RoundingMode.HALF_UP))
+        } else {
+            (interestRate * amount * days / 30 / 12).toBigDecimal(MathContext(2, RoundingMode.HALF_UP))
+        }
     }
 }
